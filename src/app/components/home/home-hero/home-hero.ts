ed66937b8e3863008aff4c6acf6dc4026869f357
Dim.Roumpekas <jimroumpi@gmail.com>
2026-06-25 23:20:52 +0300

    Add hero section to home page

---
+++ src/app/components/home/home-hero/home-hero.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home-hero',
+  imports: [RouterLink],
+  templateUrl: './home-hero.html',
+  styleUrl: './home-hero.css',
+})
+export class HomeHero {}
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HomeHero } from "./home-hero/home-hero";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, HomeHero],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
