6b71633d79294c3acccab535fe58a6ffd98d051e
Dim.Roumpekas <jimroumpi@gmail.com>
2026-06-30 14:03:41 +0300

    fix: resolve product details routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+
 import { Home } from './components/home/home';
 import { ProductList } from './components/product-list/product-list';
+import { ProductDetails } from './components/product-details/product-details';
 import { Cart } from './components/cart/cart';
 import { Discounts } from './components/discounts/discounts';
 import { ContactForm } from './components/contact-form/contact-form';
@@ -8,17 +10,42 @@ import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { Profile } from './components/profile/profile';
 import { About } from './components/about/about';
-import { authGuard } from '../guards/auth.guard';
 import { MyOrders } from './components/my-orders/my-orders';
 import { AdminProducts } from './components/admin-products/admin-products';
+
+import { authGuard } from '../guards/auth.guard';
 import { adminGuard } from '../guards/admin.guard';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'products', component: ProductList },
-  { path: 'discounts', component: Discounts },
-  { path: 'about', component: About },
-  { path: 'contact-form', component: ContactForm },
+  {
+    path: '',
+    component: Home,
+  },
+
+  {
+    path: 'products',
+    component: ProductList,
+  },
+
+  {
+    path: 'products/:id',
+    component: ProductDetails,
+  },
+
+  {
+    path: 'discounts',
+    component: Discounts,
+  },
+
+  {
+    path: 'about',
+    component: About,
+  },
+
+  {
+    path: 'contact-form',
+    component: ContactForm,
+  },
 
   {
     path: 'cart',
@@ -54,5 +81,8 @@ export const routes: Routes = [
     canActivate: [adminGuard],
   },
 
-  { path: '**', redirectTo: '' },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
