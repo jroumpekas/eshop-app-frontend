69f342a88b204f79c65eea2e6aa2b1fd31fda778
Dim.Roumpekas <jimroumpi@gmail.com>
2026-09-02 00:32:21 +0300

    fix(env): remove duplicate https protocol prefix from apiUrl

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https:// https://eshop-springboot-backend.onrender.com/api'
+  apiUrl: 'https://eshop-springboot-backend.onrender.com/api'
 };
